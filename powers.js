e79fb122bc05074b84e20413798dfc64287358e5
Trinity Terry <trinitycterry@gmail.com>
2020-01-20 11:40:33 -0600

    Added activateFLight action

--- powers.js
+++ powers.js
@@ -1,3 +1,6 @@
-document.querySelector("#activate-flight").addEventListener()
+let flightHandleFunction = () => {
+    console.log("button clicked");
+    document.getElementById("flight").classList.remove("disabled");
+}
 
-
+document.querySelector("#activate-flight").addEventListener("click", flightHandleFunction);
