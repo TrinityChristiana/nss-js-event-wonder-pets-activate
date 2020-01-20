08c565beafc2576f44e52bc25a8175116c577deb
Trinity Terry <trinitycterry@gmail.com>
2020-01-20 12:10:50 -0600

    finished excercise

--- powers.js
+++ powers.js
@@ -1,6 +1,25 @@
-let flightHandleFunction = () => {
-    console.log("button clicked");
-    document.getElementById("flight").classList.remove("disabled");
+let activateFunction = (activateId) => {
+    let activatePower = document.getElementById(`${activateId}`);
+    activatePower.classList.remove("disabled");
+    activatePower.classList.add("enabled");
 }
 
-document.querySelector("#activate-flight").addEventListener("click", flightHandleFunction);
+let changeAll = (remove, add) => {
+    let allPowers = document.querySelectorAll(".power");
+    allPowers.forEach(element => {
+        element.classList.remove(`${remove}`);
+        element.classList.add(`${add}`);
+    });
+}
+
+
+document.querySelector("#activate-flight").addEventListener("click", () => activateFunction("flight"));
+
+document.querySelector("#activate-mindreading").addEventListener("click", () => activateFunction("mindreading"));
+
+document.querySelector("#activate-xray").addEventListener("click", () => activateFunction("xray"));
+
+document.querySelector("#activate-all").addEventListener("click", () => changeAll("disabled", "enabled" ));
+
+document.querySelector("#deactivate-all").addEventListener("click", () => changeAll("enabled", "disabled"));
+
